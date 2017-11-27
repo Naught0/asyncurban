5f1c1f3ae4b902a365e65fcd9484923e039b712b
James <naught0@github.com>
2017-11-27 03:12:26 -0500

    update pypi pkg

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'AsyncUrban',
     packages = ['AsyncUrban'],
-    version = '0.1.1',
+    version = '0.1.2',
     description = 'An asynchronous wrapper around the UrbanDictionary API.',
     author = 'James E',
     author_email = 'naught0@github.com',
     url = 'https://github.com/naught0/AsyncUrban',
-    download_url = 'https://github.com/Naught0/AsyncUrban/archive/0.1.1.tar.gz',
+    download_url = 'https://github.com/Naught0/AsyncUrban/archive/0.1.2.tar.gz',
     keywords = ['dictionary', 'urban', 'urbandictionary', 'define'],
     classifiers = [],
 )
