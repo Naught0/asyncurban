5707461da67a8092f4dfbf2140c184d2d17ab97b
James <naughtsee@gmail.com>
2017-12-04 03:52:56 -0500

    add readme to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
+from os import path
 from distutils.core import setup
+
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name = 'AsyncUrban',
     packages = ['AsyncUrban'],
-    version = '0.1.2',
+    version = '0.1.3',
     description = 'An asynchronous wrapper around the UrbanDictionary API.',
     author = 'James E',
     author_email = 'naught0@github.com',
     url = 'https://github.com/naught0/AsyncUrban',
-    download_url = 'https://github.com/Naught0/AsyncUrban/archive/0.1.tar.gz',
+    download_url = 'https://github.com/Naught0/AsyncUrban/archive/0.1.3.tar.gz',
     keywords = ('dictionary', 'urban', 'urbandictionary', 'define'),
     classifiers = [],
 )
