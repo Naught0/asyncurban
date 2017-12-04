a515241652b49119af6f6cf269a4cf6b5da94493
James <naughtsee@gmail.com>
2017-12-04 04:22:32 -0500

    added a tad to setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 from os import path
 from distutils.core import setup
 
-
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name = 'AsyncUrban',
-    packages = ['AsyncUrban'],
-    version = '0.1.4',
-    description = 'An asynchronous wrapper around the UrbanDictionary API.',
-    long_description = long_description,
-    author = 'James E',
-    author_email = 'naught0@github.com',
-    url = 'https://github.com/naught0/AsyncUrban',
-    download_url = 'https://github.com/Naught0/AsyncUrban/archive/0.1.4.tar.gz',
-    keywords = ('dictionary', 'urban', 'urbandictionary', 'define'),
-    classifiers = [],
+    name='AsyncUrban',
+    packages=['AsyncUrban'],
+    version='0.1.4',
+    description='An asynchronous wrapper around the UrbanDictionary API.',
+    long_description=long_description,
+    author='James E',
+    author_email='naught0@github.com',
+    url='https://github.com/naught0/AsyncUrban',
+    download_url='https://github.com/Naught0/AsyncUrban/archive/0.1.4.tar.gz',
+    keywords=('dictionary', 'urban', 'urbandictionary', 'define'),
+    install_requires=['aiohttp'],
+    python_requires='>=3.5',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ]
 )
