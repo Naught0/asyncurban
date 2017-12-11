c608f1f1a5c4b159da9d496f1b2e06df24358007
James <naught0@github.com>
2017-12-11 12:36:20 -0500

    attempt to move to auto-versioning

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
-from os import path
-from distutils.core import setup
+from setuptools import setup
 
-long_description = ''
 with open('README.rst') as f:
     long_description = f.read()
 
 setup(
     name='asyncurban',
     packages=['asyncurban'],
-    version='0.1.9',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    license='MIT',
     description='An asynchronous wrapper around the UrbanDictionary API.',
     long_description=long_description,
     author='James E',
     author_email='naught0@github.com',
     url='https://github.com/naught0/asyncurban',
-    download_url='https://github.com/Naught0/asyncurban/archive/0.1.9.tar.gz',
+    download_url='https://github.com/naught0/asyncurban/archive/0.1.9.tar.gz',
     keywords=('dictionary', 'urban', 'urbandictionary', 'define'),
     install_requires=['aiohttp'],
     python_requires='>=3.5',
